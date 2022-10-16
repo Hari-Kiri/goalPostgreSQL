0324c07e8a24b103ad95ae9d4faad8c522b79a57
@Hari-Kiri <pratamahari89@gmail.com>
2022-10-16 21:10:13 +0700

    Refactoring PgSelect function with separate single column and multiple column query.

--- goalPostgreSQL.go
+++ goalPostgreSQL.go
@@ -10,7 +10,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// PostgreSQL connection initialize
+// Init postgres connection.
 func PgPoolConnect(databaseURL string) (*pgxpool.Pool, pgtype.Text, error) {
 	var pgVersionNil pgtype.Text
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
@@ -19,7 +19,7 @@ func PgPoolConnect(databaseURL string) (*pgxpool.Pool, pgtype.Text, error) {
 		// log.Fatalln("Unable to connect to database: ", errorConnection)
 		return nil, pgVersionNil, fmt.Errorf("unable to connect to database: %q", errorConnection)
 	}
-	// Logging to console
+	// Get postgres version
 	var postgreVersion pgtype.Text
 	errorSelectVersion := connectionPool.QueryRow(context.Background(), "SELECT VERSION()").Scan(&postgreVersion)
 	if errorSelectVersion != nil {
@@ -38,6 +38,57 @@ func PgClose(connectionPool *pgxpool.Pool) {
 // Arguments should be referenced positionally from the SQL string as $1, $2, etc.
 func PgSelect(connectionPool *pgxpool.Pool, columns []string, table string,
 	condition string, inputParameters ...any) ([]map[string]interface{}, error) {
+	/* Column 0 (error!!!) */
+	if len(columns) == 0 {
+		return nil, fmt.Errorf("no columns selected: %q", columns)
+	}
+	/* Single column query */
+	if len(columns) == 1 {
+		// Execute query
+		query := "SELECT " + columns[0] + " FROM " + table + " " + condition
+		rows, errorGetRows := connectionPool.Query(context.Background(), query, inputParameters...)
+		if errorGetRows != nil {
+			return nil, fmt.Errorf(
+				"PostgreSQL select query failed: syntax %q, query parameters %q, error %s",
+				query, inputParameters, errorGetRows)
+		}
+		defer rows.Close()
+		// Make map string interface array variable to hold this function result
+		result := make([]map[string]interface{}, 0)
+		// Iterate query result
+		for rows.Next() {
+			// Make temporary interface to store PostgreSQL query result value
+			values := make([]interface{}, len(columns))
+			// Every values returned from PostgreSQL query assign to a string pointer
+			// and all the memory adresses store in temporary interface for further process
+			for index := range columns {
+				var stringPointer any
+				values[index] = &stringPointer
+			}
+			// Scan rows from PostgreSQL query result
+			errorGetRows = rows.Scan(values...)
+			if errorGetRows != nil {
+				return nil, fmt.Errorf("failed to scan rows: %s", errorGetRows)
+			}
+			// Make map string interface variable to store temporary interface values
+			mapStringInterface := make(map[string]interface{})
+			// Read every pointer value from temporary interface then store all the data
+			// to map string interface variable
+			for index, value := range values {
+				pointer := reflect.ValueOf(value)
+				queryResult := pointer.Interface()
+				if pointer.Kind() == reflect.Ptr {
+					queryResult = pointer.Elem().Interface()
+				}
+				mapStringInterface[columns[index]] = queryResult
+			}
+			// Store all data from map string interface variable
+			// to map string interface array variable
+			result = append(result, mapStringInterface)
+		}
+		return result, nil
+	}
+	/* Multiple columns query */
 	// Extract columns parameter to syntax string
 	var columnString strings.Builder
 	// Get last column from columns parameter
